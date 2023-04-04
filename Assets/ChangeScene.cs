65cccc2ba442d7722c213d042db64a7a066e0170
GintersPelmanis <ginterspelmanis@gmail.com>
2023-04-04 17:17:36 +0300

    Added more scenes , reverted unity editor version update

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
     public void NextScene(string SceneName)
     {
-        EditorSceneManager.LoadScene(SceneName);
+        SceneManager.LoadScene(SceneName);
     }
-
 }
