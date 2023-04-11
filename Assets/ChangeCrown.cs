55e549756c7c7e885b9740e544528350320513e4
GintersPelmanis <ginterspelmanis@gmail.com>
2023-04-11 16:10:56 +0300

    Fixed Bug with Crown models not changing when the prefab was changed

--- Assets/ChangeCrown.cs
+++ Assets/ChangeCrown.cs
@@ -1,4 +1,4 @@
-
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using DanielLochner.Assets.SimpleScrollSnap;
@@ -25,6 +25,29 @@ public class ChangeCrown : MonoBehaviour
             1 => FacePrefab_2,
             _ => FacePrefab_1,
         };
+
+        List<ARFace> faces = new List<ARFace>();
+        foreach (ARFace face in FaceManager.trackables)
+        {
+            faces.Add(face);
+        }
+
+
+        foreach (ARFace face in faces)
+        {
+            if (face.transform.childCount > 0)
+            {
+                Destroy(face.transform.GetChild(0).gameObject);
+            }
+            GameObject newModel = Instantiate(NewFacePrefab, face.transform);
+            newModel.transform.localPosition = Vector3.zero;
+            newModel.transform.localRotation = Quaternion.identity;
+            newModel.transform.localScale = Vector3.one;
+        }
+
+
         FaceManager.facePrefab = NewFacePrefab;
     }
+
+
 }
