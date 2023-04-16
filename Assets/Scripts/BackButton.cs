a7a2b4ff76acafc2489dd7aea79f3ef9bb1a5f46
GintersPelmanis <ginterspelmanis@gmail.com>
2023-04-17 00:51:40 +0300

    added stack to remember what last UI scene was

--- Assets/Scripts/BackButton.cs
+++ Assets/Scripts/BackButton.cs
@@ -5,32 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class BackButton : MonoBehaviour
 {
-    private string sceneName;
+    public static Stack<string> SceneStack = new Stack<string>();
+    private string currentScene;
 
     private void Start()
     {
-        sceneName = SceneManager.GetActiveScene().name;
+        currentScene = SceneManager.GetActiveScene().name;
+        SceneStack.Push(currentScene);
+    }
+
+    private void PrevScene()
+    {
+        SceneStack.Pop();
+        if (SceneStack.Count > 0)
+            SceneManager.LoadScene(SceneStack.Peek());
+        else
+            Application.Quit();
     }
     void Update()
     {
-        if (Application.platform == RuntimePlatform.Android)
-        {
-
-
-            if (Input.GetKey(KeyCode.Escape))
-            {
-                if (sceneName == "StartScene")
-                {
-                    Application.Quit();
-                }
-                if (sceneName == "CrownInfo")
-                {
-                    SceneManager.LoadScene("StartScene");
-                }
-                SceneManager.LoadScene("StartScene");
-            }
-
-
-        }
+        if (Application.platform == RuntimePlatform.Android && Input.GetKey(KeyCode.Escape))
+            PrevScene();
     }
 }
